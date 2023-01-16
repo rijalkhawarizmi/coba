270685af914691b33984a6f5c94c79b390edae2e
muhammadrijal <muhammad.rijal@fitaja.id>
2023-01-16 21:15:46 +0700

    main

--- coba/lib/main.dart
+++ coba/lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('main'),
+    );
   }
 }
