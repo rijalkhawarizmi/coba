7d79a263230f183b1319d7c928ca49f521cddc66
rijalkhawarizmi <rijalkhawarizmi@gmail.com>
2023-01-16 21:49:08 +0700

    pul

--- coba/lib/main.dart
+++ coba/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('gas kan'),
+      child: Text('pullll kan'),
     );
   }
 }
